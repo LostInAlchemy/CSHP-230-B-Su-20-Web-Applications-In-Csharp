6092338abfd27d8dbd3cae6518a6cd9e00fd2cd1
grumpyate1@msn.com <grumpyate1@msn.com>
2020-06-28 23:45:19 -0700

    Week 1

---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please select a response")]
+        public bool? WillAttend { get; set; }
+    }
+}

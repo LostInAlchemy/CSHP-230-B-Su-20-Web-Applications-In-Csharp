a14ec04d7137945997de1be3d2b5202695c99c7d
grumpyate1@msn.com <grumpyate1@msn.com>
2020-06-30 00:23:09 -0700

    Week 1 Complete

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HelloWorld.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace HelloWorld.Controllers
@@ -8,8 +9,12 @@ namespace HelloWorld.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //int x = 1;  // add me
+            //x = x / (x - 1); // add me
             return View();
         }
+
+
 
         [HttpGet]
         public ActionResult RsvpForm()
@@ -29,35 +34,58 @@ namespace HelloWorld.Controllers
             }
         }
 
+        //public ActionResult Product()
+        //{
+        //    var myProduct = new Product
+        //    {
+        //        ProductId = 1,
+        //        Name = "Kayak",
+        //        Description = "A boat for one person",
+        //        Category = "water-sports",
+        //        Price = 200m,
+        //    };
+
+        //    ViewBag.Title = "Single Product";
+
+        //    return View(myProduct);
+        //}
+
+        //public ActionResult Products()
+        //{
+        //    var products = new Product[]
+        //    {
+        //        new Product{ ProductId = 1, Name = "First One", Price = 1.11m},
+        //        new Product{ ProductId = 2, Name="Second One", Price = 2.22m},
+        //        new Product{ ProductId = 3, Name="Third One", Price = 3.33m},
+        //        new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m},
+        //    };
+
+        //    ViewBag.Title = "Multiple Product";
+
+        //    return View(products);
+        //}
+
         public ActionResult Product()
         {
-            var myProduct = new Product
-            {
-                ProductId = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "water-sports",
-                Price = 200m,
-            };
-
-            return View(myProduct);
+            return View(productRepository.Products.First());
         }
 
         public ActionResult Products()
         {
-            var products = new Product[]
-            {
-                new Product{ ProductId = 1, Name = "First One", Price = 1.11m},
-                new Product{ ProductId = 2, Name="Second One", Price = 2.22m},
-                new Product{ ProductId = 3, Name="Third One", Price = 3.33m},
-                new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m},
-            };
-
-            return View(products);
+            return View(productRepository.Products);
         }
 
+        //protected override void OnException(ExceptionContext filterContext)
+        //{
+        //    base.OnException(filterContext);
+        //}
 
+        private IProductRepository productRepository;
 
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
 
     }
 }

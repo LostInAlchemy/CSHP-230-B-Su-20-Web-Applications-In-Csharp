0d9866d7b4a92a5898a8af3689d02371ddf0d44f
grumpyate1@msn.com <grumpyate1@msn.com>
2020-07-02 00:33:31 -0700

    Project Start

---
+++ TheLearningCenter/TheLearningCenter/Models/NewAccount.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheLearningCenter.Models
+{
+    public class NewAccount
+    {
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please confirm your password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
